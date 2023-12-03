adbdd6260a7454b100217e4f34880979b799b0f7
FotihYusupov <yusupovfotih158@gmai.com>
2023-12-03 16:27:48 +0500

    add fs function

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const { Console } = require('console');
 const dotenv = require('dotenv');
 const express = require('express');
+const fs = require('fs')
 
 dotenv.config();
 
@@ -7,11 +9,27 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip)
-    return res.send(req.ip)
+    const originalClientIp = ip.split(',')[0].trim();
+    saveToJSON(originalClientIp);
+    return res.send(originalClientIp);
+})
+
+app.get('/get-ip', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('data.json'));
+    res.json({
+        data: data
+    })
 })
 
 app.listen(process.env.PORT || 3001, () => console.log('server is run'));
+
+function saveToJSON(ipAddress) {
+    const data = JSON.parse(fs.readFileSync('data.json'));
+    const filter = data.filter(e => e.ipAddress === ipAddress)
+    if(!filter.length > 0) {
+        data.push({id: data.at(-1).id + 1 || 1, ipAddress: ipAddress})
+        fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+    }
+}
